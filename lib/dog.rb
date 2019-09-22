ddf4c0e40db4707513adef72743408767109c369
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-22 19:57:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,13 @@ class Dog
   
   attr_accessor :owner, :mood
   
+  @@all = []
+  
   def initialize(name, owner)
     @name = name 
     @owner = owner
     @mood = "nervous"
+    @@all << self
   end 
   
   def name 
