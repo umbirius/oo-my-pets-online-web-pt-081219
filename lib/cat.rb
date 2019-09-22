3ff2d6a47d5e48b3f0081e9e7541398a5bdaf96d
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-22 19:35:58 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,10 @@
 class Cat
-  # code goes here
+  
+  attr_accessor :owner
+  
+  def initialize(name,owner)
+    @name = name
+    @owner = Owner.new("#{owner}") unless Owner.all.include?(owner)
+  end 
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,36 @@
 class Owner
-  # code goes here
+  
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    self.species
+    @@all << self
+  end 
+  
+  def name
+    @name 
+  end 
+  
+  def species 
+    @species = "human"
+  end 
+  
+  def say_species 
+    return "I am a #{@species}."
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.count 
+    @@all.length 
+  end 
+  
+  def self.reset_all 
+    @@all = []
+  end 
+  
 end
