46ca3a67e10c152b217f38672dab936b3f6b8f39
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-22 20:40:04 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,6 +59,7 @@ class Owner
   def sell_pets
     self.dogs.each {|dog| dog.mood = "nervous"}
     self.cats.each {|cat| cat.mood = "nervous"}
+    self.dogs.select{|dog| dog.owner = nil}
   end 
     
   
