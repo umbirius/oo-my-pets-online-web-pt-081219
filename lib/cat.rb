1a0a394e662259e0e3812cf99e09b999fcef7d4d
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-22 19:53:37 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,11 +2,13 @@ class Cat
   
   attr_accessor :owner, :mood
   
+  @@all = []
   
   def initialize(name,owner)
     @name = name
     @owner = owner
     @mood = "nervous"
+    @@all << self
   end 
   
   def name 
